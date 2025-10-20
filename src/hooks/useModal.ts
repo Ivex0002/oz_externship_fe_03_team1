8d2ce6b4ad56322252166ef7cd0a8aa4db9968a1
ivex0002 <garam199803@gmail.com>
2025-10-20 10:12:32 +0900

    fix:빌드 안되던 문제 해결

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -26,7 +26,7 @@ export function useModal() {
   const openModal = (modalPath: string, title: string, subTitle?: string) => {
     const currentPath = location.pathname
     navigate(modalPath)
-    storeModalOpen.getState().setIsModalState({
+    storeModalOpen.getState().setModalState({
       isModalOpen: true,
       prevPath: currentPath,
       title: title,
@@ -45,7 +45,7 @@ export function useModal() {
     subTitle?: string
   ) => {
     navigate(modalPath)
-    storeModalOpen.getState().setIsModalState({
+    storeModalOpen.getState().setModalState({
       isModalOpen: true,
       title: title,
       subTitle: subTitle,

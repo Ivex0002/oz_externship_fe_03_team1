3f7922a0adab8f61399f7590d8c0527485f4e984
rimi-w <whi1230@naver.com>
2025-10-24 13:00:43 +0900

    docs: 주석 수정 및 사용법 설명 개선

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -16,10 +16,11 @@ dayjs.locale('ko')
 
 export default dayjs
 
-// 프로젝트 어디서든 import dayjs from "경로/lib/dayjs" 로 쓰면 플러그인/한글 설정이 적용된 상태로 사용됩니다.
+// 사용 시 import dayjs from "@/lib/dayjs" -> 플러그인/한글 설정이 적용된 상태로 사용가능
+// * 경로에 '@/lib/dayjs'를 사용해야 함에 유의!! (자동 import 시 'dayjs'로만 들어오는 경우가 있음)
 
 // localizedFormat 사용 예시:
-// dayjs().format("L") -> 2025.09.07;
+// dayjs().format("L") -> 2025.09.07
 // dayjs().format("LL") -> 2025년 9월 7일
 // dayjs().format("LLL") -> 2025년 9월 7일 오후 3:45
 // dayjs().format("LLLL") -> 2025년 9월 7일 일요일 오후 3:45

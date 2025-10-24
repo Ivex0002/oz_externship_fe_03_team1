63a12ab301de3b51f3e1e7cd9f0af3a45f891ebc
ivex0002 <garam199803@gmail.com>
2025-10-24 17:55:35 +0900

    fix:formatToHourMin 주석 설명 수정

--- src/hooks/useFormatDate.ts
+++ src/hooks/useFormatDate.ts
@@ -12,7 +12,7 @@ export function formatToMonthDay(isoString: string): string {
 /**
  * iso 형식의 날짜를 "nn:nn"(24시간제 시:분) 문자열로 파싱해주는 유틸 메서드
  * @param isoString iso형식 날짜 데이터
- * @returns n월 n일
+ * @returns nn(시):nn(분)
  */
 export function formatToHourMin(isoString: string): string {
   const date = new Date(isoString)

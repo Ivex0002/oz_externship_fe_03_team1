5c6a42e0204395dabc86d5671c76836cce47dde6
ivex0002 <garam199803@gmail.com>
2025-10-29 13:50:29 +0900

    fix:확인용 콘솔로그 비활성화

--- src/test/TestG.tsx
+++ src/test/TestG.tsx
@@ -1,14 +1,14 @@
-import { api } from '@/api/api'
+// import { api } from '@/api/api'
 import { BasicButton } from '@/components/basicComponents/BasicButton/BasicButton'
 
 export default function TestG() {
   const handleClick = async () => {
-    try {
-      const res = await api.v1.lectures.GET()
-      console.log(res)
-    } catch (error) {
-      console.error('API call failed:', error)
-    }
+    // try {
+    //   const res = await api.v1.lectures.GET()
+    //   console.log(res)
+    // } catch (error) {
+    //   console.error('API call failed:', error)
+    // }
   }
   return (
     <div className="mx-auto w-[567px] p-4">

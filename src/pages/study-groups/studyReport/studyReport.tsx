bd76d2b0d2a976550574f75fa99ca76e0c45f41b
Grunnodap <asjk15110@gmail.com>
2025-10-30 17:02:05 +0900

    fix : 오류 수정중

--- src/pages/study-groups/studyReport/studyReport.tsx
+++ src/pages/study-groups/studyReport/studyReport.tsx
@@ -5,7 +5,7 @@ import RecordMarkdownEditor from '@/components/studyReport/RecordMarkdownEditor'
 import RecordFileUpload from '@/components/studyReport/RecordFileUpload'
 import RecordActionButtons from '@/components/studyReport/RecordActionButtons'
 
-export function StudyRecordForm() {
+export default function StudyRecordForm() {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [file, setFile] = useState<File | null>(null)

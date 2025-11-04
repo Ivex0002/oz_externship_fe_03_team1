c100b1490287090d24c6f0c4b60cd5c865d3e8c5
ivex0002 <garam199803@gmail.com>
2025-11-04 18:09:06 +0900

    fix:테스트 환경 변경 - 모달창의 라우팅 구조 변경으로 testH의 버튼을 testModal로 이동

--- src/test/TestH.tsx
+++ src/test/TestH.tsx
@@ -1,20 +1,6 @@
-import { BasicButton } from '@/components/basicComponents/BasicButton/BasicButton'
-import { useModal } from '@/hooks/useModal'
-
 function TestH() {
-  const { openConfirm, closeModal } = useModal()
-
-  const options = {
-    message: '리더를 위임하시겠습니까?',
-    onConfirm: closeModal,
-    onCancel: closeModal,
-  }
-
-  return (
-    <div>
-      <BasicButton onClick={() => openConfirm(options)}>확인창</BasicButton>
-    </div>
-  )
+  // 확인창 라우팅 구조 문제로 TestModal로 이동
+  return <div></div>
 }
 
 export default TestH
--- src/test/TestModal.tsx
+++ src/test/TestModal.tsx
@@ -2,30 +2,45 @@ import { BasicButton } from '@/components/basicComponents/BasicButton/BasicButto
 import { useModal } from '@/hooks/useModal'
 import type { ModalType } from '@/store/storeModalOpen'
 
-const modalTestList: {
-  modalType: Exclude<ModalType, null>
-  title: string
-  subTitle?: string
-  modalProps?: Record<string, unknown>
-}[] = [
-  { modalType: 'SCHEDULE', title: '새 스케줄 추가' },
-  {
-    modalType: 'DETAIL_SCHEDULE',
-    title: '스케줄 상세',
-    modalProps: { scheduleId: 1 },
-  },
-  { modalType: 'DATE_PICKER', title: '날짜 선택' },
-  { modalType: 'LECTURE_CHOOSING', title: '강의 선택' },
-  { modalType: 'REVIEW', title: '리뷰 작성', modalProps: { studyGroupId: 1 } },
-  {
-    modalType: 'REVIEW_DETAIL',
-    title: '리뷰 상세',
-    modalProps: { studyGroupId: 1 },
-  },
-]
-
 function TestModal() {
-  const { openModal } = useModal()
+  const { openModal, closeModal } = useModal()
+
+  const options = {
+    message: '리더를 위임하시겠습니까?',
+    onConfirm: closeModal,
+    onCancel: closeModal,
+  }
+
+  const modalTestList: {
+    modalType: Exclude<ModalType, null>
+    title: string
+    subTitle?: string
+    modalProps?: Record<string, unknown>
+  }[] = [
+    { modalType: 'SCHEDULE', title: '새 스케줄 추가' },
+    {
+      modalType: 'DETAIL_SCHEDULE',
+      title: '스케줄 상세',
+      modalProps: { scheduleId: 1 },
+    },
+    { modalType: 'DATE_PICKER', title: '날짜 선택' },
+    { modalType: 'LECTURE_CHOOSING', title: '강의 선택' },
+    {
+      modalType: 'REVIEW',
+      title: '리뷰 작성',
+      modalProps: { studyGroupId: 1 },
+    },
+    {
+      modalType: 'REVIEW_DETAIL',
+      title: '리뷰 상세',
+      modalProps: { studyGroupId: 1 },
+    },
+    {
+      modalType: 'CONFIRM',
+      title: '확인',
+      modalProps: { options },
+    },
+  ]
 
   return (
     <div className="m-auto mt-20 flex flex-col gap-2">

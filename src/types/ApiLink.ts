5e2de6cb0f882cc24a2c6a03479e83652cf54168
ivex0002 <garam199803@gmail.com>
2025-11-11 17:43:38 +0900

    fix: apilink 명세서를 기준으로 최신화 - 알림, 스터디 그룹, 어드민, Presigned-url, 노트

--- src/types/ApiLink.ts
+++ src/types/ApiLink.ts
@@ -1,7 +1,3 @@
-// ===================== User =====================
-// type RoleEnum = 'admin' | 'staff' | 'user'
-// (스웨거) RoleEnum이 존재하나 어디에도 쓰이지 않음
-
 import type {
   AxiosError,
   AxiosRequestConfig,
@@ -9,6 +5,9 @@ import type {
   Method,
 } from 'axios'
 
+// ===================== User =====================
+// type RoleEnum = 'admin' | 'staff' | 'user'
+// (스웨거) RoleEnum이 존재하나 어디에도 쓰이지 않음
 // (예상) UserProfile에 RoleEnum 추가 가능성 있음
 // (스웨거) 스터디 그룹의 멤버 항목은 uuid로써 식별값을 가지지만,
 // /api/v1/users/me 에선 number 타입으로 받음
@@ -25,27 +24,32 @@ type UserProfile = {
 }
 
 // ===================== Notification =====================
-type NotificationTypeEnum =
-  | 'APPLICATIONS_CREATED'
-  | 'APPLICATION_STATUS_APPROVAL'
-  | 'APPLICATION_STATUS_REJECTION'
-  | 'STUDY_MEMBER_JOINED'
-  | 'STUDY_REVIEW_REQUEST'
-  | 'STUDY_SCHEDULE_UPCOMING'
-  | 'STUDY_SCHEDULE_TODAY'
-  | 'STUDY_RECORD_CREATED'
-  | 'SYSTEM'
-  | 'CUSTOM'
+// type NotificationTypeEnum =
+//   | 'APPLICATIONS_CREATED'
+//   | 'APPLICATION_STATUS_APPROVAL'
+//   | 'APPLICATION_STATUS_REJECTION'
+//   | 'STUDY_MEMBER_JOINED'
+//   | 'STUDY_REVIEW_REQUEST'
+//   | 'STUDY_SCHEDULE_UPCOMING'
+//   | 'STUDY_SCHEDULE_TODAY'
+//   | 'STUDY_RECORD_CREATED'
+//   | 'SYSTEM'
+//   | 'CUSTOM'
 
 type Notification = {
   id: number
-  user_id: number
-  type_display: string
-  content: string
-  type: NotificationTypeEnum
+  message: string
   is_read: boolean
-  back_url_link: string
-  user: number
+  type: string
+  back_link_url: string
+  created_at: string
+}
+
+type NotiStudyJoinPost = {
+  // api 명세서상 number 타입
+  // 추후 변경 가능성 매우 높음
+  study_groups_id: number
+  user_id: number
 }
 
 // ===================== Lecture =====================
@@ -78,6 +82,15 @@ type Lecture = {
 // api 명세서를 기준으로 작성 - 이후 변경 가능성 있음
 export type StudyGroupStatus = 'PENDING' | 'ONGOING' | 'ENDED'
 
+// 스터디 그룹 목록조회, 스터디 그룹 상세조회 => 강의 타입이 상이함
+type StudyLecture = {
+  uuid: string
+  thumbnail_img_url: string
+  title: string
+  instructor: string
+  url_link: string
+}
+
 type StudyGroup = {
   uuid: string
   name: string
@@ -87,15 +100,12 @@ type StudyGroup = {
   start_at: string
   end_at: string
   status: StudyGroupStatus
-  // is_leader 필드가 왜 여기 있는가?
-  // 어차피 멤버 필드에 is_leader: boolean 있다면 현재 접속중인 유저와 비교하고 bool값을 뽑을수 있지 않나?
-  is_leader: boolean
-  lectures: Lecture[]
+  lectures: StudyLecture[]
 }
 
 // api 명세서를 기준으로 작성 - 이후 변경 가능성 있음
 type Member = {
-  id: number
+  uuid: number
   nickname: string
   is_leader: boolean
 }
@@ -106,33 +116,83 @@ type StudyGroupDetail = StudyGroup & {
 }
 
 // api 명세서를 기준으로 작성 - 이후 변경 가능성 있음
-//
 type StudyGroupPostLecture = {
-  id: number
+  uuid: string
   title: string
   instructor: string
 }
 
 type StudyGroupPost = {
   name: string
+  introduction: string
   profile_img_url: string
-  max_headcount: number
   start_at: string
   end_at: string
-  introduction: string
+  max_headcount: number
+  status: string
   // api 명세서를 기준으로 작성 - 이후 변경 가능성 있음
   // (스웨거) uuid[] 라고 기입되어 있음
   lectures: StudyGroupPostLecture[]
+}
+
+// api명세서:lectures타입 기입 x
+// StudyGroupPostLecture 로 임시 대체
+type StudyGroupPut = {
+  name: string
+  introduction: string
+  profile_img_url: string
+  start_at: string
+  end_at: string
+  lectures: StudyGroupPostLecture[]
+  max_headcount: number
 }
 
 // ===================== Study:Group-Admin =====================
 // 관리자용이랑 일반 스터디 그룹 타입 형태가 다른 이유?
 // 이미 uuid 있는데 다시 id를 작성하는 이유?
 // 데이터의 유니크 키값으로 id를 쓰는건가 아니면 리더의 유저 id인가?
-// 딱봐도 나중에 수정될거 같아서
-type AdminStudyGroupDetail = { id: number } & StudyGroupDetail
+// **추후에 수정 가능성 매우 높음**
 
-type AdminStudyGroupList = Partial<AdminStudyGroupDetail>
+// 기존 멤버 타입에서 uuid 누락됨
+type AdminStudyGroupDetailMember = {
+  nickname: string
+  is_leader: boolean
+}
+// 기존 StudyLecture 에서 uuid 누락됨
+type AdminStudyGroupDetailLecture = {
+  thumbnail_img_url: string
+  title: string
+  instructor: string
+  url_link: string
+}
+type AdminStudyGroupDetail = {
+  id: number
+  uuid: string
+  name: string
+  current_headcount: number
+  max_headcount: number
+  members: AdminStudyGroupDetailMember[]
+  profile_img_url: string
+  start_at: string
+  end_at: string
+  status: string
+  lectures: AdminStudyGroupDetailLecture[]
+  created_at: string
+  updated_at: string
+}
+
+type AdminStudyGroup = {
+  id: number
+  name: string
+  current_headcount: number
+  max_headcount: number
+  profile_img_url: string
+  start_at: string
+  end_at: string
+  status: string
+  created_at: string
+  updated_at: string
+}
 
 // ===================== Study:Review =====================
 type ReviewEnum =
@@ -169,9 +229,83 @@ type StudyReviewPost = {
 type File = {
   file_name: string
   content_type: string
+  file_size: number
 }
-type PresignedURL = {
+type PresignedURLReq = {
   files: File[]
+}
+
+type PresignedURLRes = {
+  file_name: string
+  key: string
+  url: string
+  fields: {
+    key: string
+    policy: string
+    'x-amz-signature': string
+  }
+  file_url: string
+  expires_in: 300
+}
+
+// ===================== Study:Note =====================
+type NoteFile = {
+  type: string
+  url: string
+  filename: string | null
+}
+// **추후에 Author id 항목 uuid로 변경될 가능성 매우 높음**
+type NoteAuthor = {
+  id: number
+  nickname: string
+  profile_image_url: string
+}
+
+type AISummary = {
+  available: boolean
+  created_at: string
+  format: string
+  body: string
+}
+
+type Note = {
+  id: number
+  group_id: string
+  title: string
+  author: NoteAuthor
+  content_md: string
+  attachments: NoteFile[]
+  ai_summary: AISummary
+  created_at: string
+  updated_at: string
+}
+
+type NoteGetByGroupRes = {
+  id: number
+  title: string
+  author: NoteAuthor
+  created_at: string
+}
+
+type NotePost = {
+  group_id: string
+  title: string
+  content_md: string
+  attachments: NoteFile[]
+}
+
+type NotePostRes = {
+  id: number
+  group_id: string
+  title: string
+  author: NoteAuthor
+  created_at: string
+}
+
+type NotePatchReq = {
+  title: string
+  content_md: string
+  attachments: NoteFile[]
 }
 
 // ===================== Study:Schedule =====================
@@ -219,6 +353,16 @@ export type Pagination<T> = {
 }
 
 // ===================== etc =====================
+export type BaseResWithGeneric<T> = {
+  status: number
+  message: string
+  data: T | null
+  error?: {
+    code: string
+    detail: string
+  }
+}
+
 export type BaseResponse = {
   status: number
   message: string
@@ -238,6 +382,9 @@ export type BaseResponse = {
  *
  * 스웨거
  * @see https://api.ozcoding.site/api/schema/swagger-ui/#/
+ *
+ * API 명세서
+ * @see https://www.notion.so/API-27acaf5650aa81f3b3fde1ecf0aeb032
  */
 export type ApiLinks = {
   v1: {
@@ -252,7 +399,25 @@ export type ApiLinks = {
       }
     }
     notifications: {
+      // 알림쪽 api 명세서 매우 미흡함
+      // 단일문서에서도 GET-POST 혼용
+      // 엔드포인트 분리 X
+      // 예정 스케줄 알림 생성 데이터 필드 누락
       GET: () => { res: Pagination<Notification> }
+      read$all: {
+        PATCH: () => { res: BaseResponse }
+      }
+      (notification_id: number): {
+        PATCH: () => { res: BaseResponse }
+      }
+      study: {
+        join: {
+          POST: (req: NotiStudyJoinPost) => { res: BaseResponse }
+        }
+        review$request: {
+          POST: () => { res: BaseResponse }
+        }
+      }
     }
     lectures: {
       // (스웨거) 검색, 필터, 페이지네이션 기능 누락
@@ -267,12 +432,21 @@ export type ApiLinks = {
     }
     studies: {
       groups: GroupApi
+      notes: NoteApi
       admin: {
         groups: {
-          GET: () => { res: Pagination<AdminStudyGroupList> }
+          // api 명세서 : BaseResWithGeneric<AdminStudyGroupList>
+          // 스웨거 : Pagination<AdminStudyGroupList>
+          // 서로 타입이 상충함
+          /**
+           * @queries {finished:boolean, sort:string, limit:int, offset:int}
+           */
+          GET: () => {
+            res: BaseResWithGeneric<{ study_groups: AdminStudyGroup[] }>
+          }
 
           (group_uuid: string): {
-            GET: () => { res: AdminStudyGroupDetail }
+            GET: () => { res: BaseResWithGeneric<AdminStudyGroupDetail> }
           }
         }
       }
@@ -283,8 +457,8 @@ export type ApiLinks = {
       }
     }
     chat: {
-      chatrooms: {
-        GET: () => { res: ChatRoom[] }
+      rooms: {
+        GET: () => { res: BaseResWithGeneric<ChatRoom[]> }
         (studyGroupId: string): {
           messages: {
             // 해당 요청 에러시 에러 객체가 아닌 일반 메시지로 응답이 옴(403)
@@ -299,55 +473,61 @@ export type ApiLinks = {
 }
 
 type GroupApi = {
+  /**
+   * @queries {status, page, search}
+   * @example
+   * ```ts
+   * api.v1.studies.groups.GET(undefined, {status:”PENDING” | ”ONGOING” | ”ENDED” , page:number, search:string})
+   * ```
+   */
   GET: () => { res: Pagination<StudyGroup> }
-  POST: (req: StudyGroupPost) => { res: BaseResponse }
+  POST: (req: StudyGroupPost) => {
+    res: BaseResWithGeneric<StudyGroupPost & { uuid: string }>
+  }
 
-  // 파일 보내기 전 요청 url을 먼저 받는게 맞는거 같은데
-  // res 타입이 {"files": [{"file_name": "string","content_type": "string"}]} 으로 오는게 맞나?
-  // 아무리 봐도 presigned-url 데이터 구조는 아닌거 같음
   presigned$url: {
-    POST: (req: PresignedURL) => { res: PresignedURL }
+    POST: (req: PresignedURLReq) => {
+      res: BaseResWithGeneric<PresignedURLRes[]>
+    }
   }
 
   (group_uuid: string): {
-    GET: () => { res: StudyGroupDetail }
-    delegate$leader: {
-      // userProfile을 제외한 모든 필드에서 유저 id는 uuid 형식임
-      POST: (req: { target_member_uuid: string }) => {
-        res: {
-          // 어째서 타겟은 uuid이고 이전, 신규/이전 리더는 number인가?
-          // 동일한 유저 필드를 참조하는 것이라면 둘 중 하나로 통일하는것이 맞음
-          // 현재 member~로 지칭되는것은 전부 uuid 이고, user~ 로 지칭되는 것은 id로 서로 데이터 구조가 다름
-          target_member_uuid: string
-          previous_leader_id: number
-          new_leader_id: number
-        }
-      }
-    }
+    GET: () => { res: BaseResWithGeneric<StudyGroupDetail> }
+    PUT: (req: StudyGroupPut) => { res: BaseResponse }
+    // /api/vi/studies/groups/{uuid}/members/{member_id} PATCH 로 이동
+    // 문서 내부에서 서로 다른 엔드포인트가 혼용되고 있음
+    // 그러나 데이터 구조상 POST보다는 데이터 일부만 수정하는 PATCH가 알맞고,
+    // 굳이 새로운 엔드포인트를 팔 이유가 없기에
+    // members/{member_id} PATCH 가 합당한 조치라고 판단하여 이동 조치함
+    // delegate$leader: {
+    //   // userProfile을 제외한 모든 필드에서 유저 id는 uuid 형식임
+    //   POST: (req: { target_member_uuid: string }) => {
+    //     res: {
+    //       // 어째서 타겟은 uuid이고 이전, 신규/이전 리더는 number인가?
+    //       // 동일한 유저 필드를 참조하는 것이라면 둘 중 하나로 통일하는것이 맞음
+    //       // 현재 member~로 지칭되는것은 전부 uuid 이고, user~ 로 지칭되는 것은 id로 서로 데이터 구조가 다름
+    //       target_member_uuid: string
+    //       previous_leader_id: number
+    //       new_leader_id: number
+    //     }
+    //   }
+    // }
     leave: { DELETE: () => { res: BaseResponse } }
     // 다른 곳에선 member : uuid 형식인데 여기는 왜 number 타입으로 받나?
-    members: { (member_id: number): { DELETE: () => { res: BaseResponse } } }
-    notes: {
-      // (스웨거) res 타입 아직 안나옴
-      GET: () => { res: BaseResponse }
-
-      // (스웨거) /api/v1/studies/notes 에 반드시 그룹 uuid가 링크에 포함되어야 하는 파트인데도 미포함 되어있음
-      // 아무리 봐도 반드시 링크에 uuid 첨부 형식인거 같아 스웨거와 다르게 작업함
-      // 현재 GET메서드 하나만 uuid 포함이고, 나머지는 전부 studies 직후에 연결되고 있음
-      POST: () => { res: BaseResponse }
-
-      (note_id: number): {
-        GET: () => { res: BaseResponse }
+    members: {
+      (member_id: number): {
         DELETE: () => { res: BaseResponse }
+        PATCH: () => { res: BaseResponse }
       }
-
-      // 위의 presigned-url와 동일한 문제를 가지고 있음
-      // (스웨거) 또한 링크 주소 상에 note_id 누락 되어있음
-      // note_id 반드시 포함이면 처음 노트 작성 도중 이미지 첨부는 어떻게 하나?
-      // 작성 시작시 바로 POST요청 보내고 임시저장 만든다음 id 받아와서 PUT이나 PATCH 방식으로 하나?
-      // 파일 저장용 데이터 필드 구축하고 거기서 참조 형식으로 작성되나?
-      presigned$url: {
-        POST: (req: PresignedURL) => { res: PresignedURL }
+    }
+    notes: {
+      GET: () => {
+        res: BaseResWithGeneric<
+          Pagination<Partial<NoteGetByGroupRes>> & {
+            order: string
+            group_id: string
+          }
+        >
       }
     }
     reviews: {
@@ -367,6 +547,22 @@ type GroupApi = {
         PUT: (req: StudyReview) => { res: StudyReview }
         PATCH: (req: StudyReview) => { res: StudyReview }
       }
+    }
+  }
+}
+
+type NoteApi = {
+  POST: (req: NotePost) => { res: BaseResWithGeneric<NotePostRes> }
+
+  (note_id: number): {
+    GET: () => { res: BaseResWithGeneric<Note> }
+    PATCH: (req: NotePatchReq) => { res: BaseResWithGeneric<Note> }
+    DELETE: () => { res: BaseResponse }
+  }
+
+  presigned$url: {
+    POST: (req: PresignedURLReq) => {
+      res: BaseResWithGeneric<PresignedURLRes[]>
     }
   }
 }

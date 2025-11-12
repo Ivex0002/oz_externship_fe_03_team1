fb0754922679ac94c15ba4cd1db36e83e2e15d0f
rimi-w <whi1230@naver.com>
2025-11-12 11:37:46 +0900

    feat: 리뷰 쿼리 생성 (#122)

---
+++ src/hooks/api/queries/useQueryReview.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+import { queryKeys } from '../queryKeys'
+import { api } from '@/api/api'
+
+interface ReviewParams {
+  page: number
+  page_size: number
+  groupId: string
+}
+
+export const useQueryReview = (params: ReviewParams) => {
+  return useQuery({
+    queryKey: queryKeys.studies.groups.reviews(params.groupId),
+    queryFn: () =>
+      api.v1.studies
+        .groups(params.groupId)
+        .reviews.GET(undefined, {
+          params: { page: params.page, page_size: params.page_size },
+        }),
+  })
+}

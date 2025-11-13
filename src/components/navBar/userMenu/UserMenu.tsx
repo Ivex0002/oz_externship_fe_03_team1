2a3140df22980f9d1e3f4e73fa2973cc2d1f57fc
ivex0002 <garam199803@gmail.com>
2025-11-13 10:43:37 +0900

    feat:유저 정보 받아오기, 엑세스 토큰 받아오기 로직 추가

--- src/components/navBar/userMenu/UserMenu.tsx
+++ src/components/navBar/userMenu/UserMenu.tsx
@@ -1,8 +1,54 @@
+import { useEffect } from 'react'
 import { LoginedMenu } from './LoginedMenu'
 import { LogoutedMenu } from './LogoutedMenu'
+import { refreshAccessToken } from '@/api/refreshAccessToken'
+import { storeAccessToken } from '@/store/storeAccessToken'
+import { toast } from 'react-toastify'
+import { storeUser } from '@/store/storeUser'
+import { api } from '@/api/api'
 
 export const UserMenu = () => {
-  // 로그인 확인 로직 필요
-  const isLogined = true
-  return isLogined ? <LoginedMenu /> : <LogoutedMenu />
+  const { user } = storeUser()
+
+  return (
+    <>
+      <RefreshComp />
+      <GetMeComp />
+      {user ? <LoginedMenu /> : <LogoutedMenu />}
+    </>
+  )
+}
+
+const RefreshComp = () => {
+  const { setAccessToken } = storeAccessToken()
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const AccessToken = await refreshAccessToken()
+        if (AccessToken) setAccessToken(AccessToken)
+      } catch (error) {
+        toast.error(`${error}`)
+      }
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return null
+}
+
+const GetMeComp = () => {
+  const { accessToken } = storeAccessToken()
+  const { setUser } = storeUser()
+  useEffect(() => {
+    ;(async () => {
+      try {
+        if (!accessToken) return
+        const user = await api.v1.users.me.GET()
+        setUser(user)
+      } catch (error) {
+        toast.error(`${error}`)
+      }
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [accessToken])
+  return null
 }

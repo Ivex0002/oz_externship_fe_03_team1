b97bb2876af41dad529fafedeca173f101f29a50
ivex0002 <garam199803@gmail.com>
2025-11-13 11:05:22 +0900

    fix:비동기 유즈 이펙트 리팩터링

--- src/components/navBar/userMenu/UserMenu.tsx
+++ src/components/navBar/userMenu/UserMenu.tsx
@@ -1,11 +1,10 @@
-import { useEffect } from 'react'
 import { LoginedMenu } from './LoginedMenu'
 import { LogoutedMenu } from './LogoutedMenu'
 import { refreshAccessToken } from '@/api/refreshAccessToken'
 import { storeAccessToken } from '@/store/storeAccessToken'
-import { toast } from 'react-toastify'
 import { storeUser } from '@/store/storeUser'
 import { api } from '@/api/api'
+import { useAsyncEffect } from '@/hooks/useAsyncEffect'
 
 export const UserMenu = () => {
   const { user } = storeUser()
@@ -21,34 +20,26 @@ export const UserMenu = () => {
 
 const RefreshComp = () => {
   const { setAccessToken } = storeAccessToken()
-  useEffect(() => {
-    ;(async () => {
-      try {
-        const AccessToken = await refreshAccessToken()
-        if (AccessToken) setAccessToken(AccessToken)
-      } catch (error) {
-        toast.error(`${error}`)
-      }
-    })()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+
+  useAsyncEffect(
+    async () => await refreshAccessToken(),
+    (token) => token && setAccessToken(token),
+    []
+  )
   return null
 }
 
 const GetMeComp = () => {
   const { accessToken } = storeAccessToken()
   const { setUser } = storeUser()
-  useEffect(() => {
-    ;(async () => {
-      try {
-        if (!accessToken) return
-        const user = await api.v1.users.me.GET()
-        setUser(user)
-      } catch (error) {
-        toast.error(`${error}`)
-      }
-    })()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [accessToken])
+
+  useAsyncEffect(
+    async () => {
+      if (!accessToken) return
+      return await api.v1.users.me.GET()
+    },
+    (user) => user && setUser(user),
+    [accessToken]
+  )
   return null
 }
---
+++ src/hooks/useAsyncEffect.ts
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+
+/**
+ * 비동기 effect를 공통화한 훅
+ *
+ * @param storeSelector 구독할 스토어 (필요 없으면 null)
+ * @param asyncFn 실행할 비동기 함수 (필수)
+ * @param onSuccess 성공 시 실행할 콜백 (res를 인자로 받음)
+ * @param deps 의존성 배열
+ */
+export const useAsyncEffect = <S, R>(
+  asyncFn: (storeValue?: S) => Promise<R>,
+  onSuccess: (res: R) => void,
+  deps: React.DependencyList
+) => {
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const res = await asyncFn()
+        if (res !== undefined) onSuccess(res)
+      } catch (error) {
+        toast.error(`${error}`)
+      }
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps)
+}

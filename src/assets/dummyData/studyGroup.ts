6a9159f975c59b0b1a6d96dbda5fdcd9055aca8f
Grunnodap <asjk15110@gmail.com>
2025-11-15 19:39:03 +0900

    fix : 빌드 오류 제거 중(#131)

--- src/assets/dummyData/studyGroup.ts
+++ src/assets/dummyData/studyGroup.ts
@@ -1,7 +1,7 @@
 import type { StudyGroup } from '@/types/StudyGroupTypes'
 
 export const studyGroup: StudyGroup = {
-  id: '1',
+  uuid: '1',
   name: 'string',
   profile_img_url: 'string',
   current_headcount: 5,
@@ -10,11 +10,5 @@ export const studyGroup: StudyGroup = {
   is_leader: false,
   start_at: '2025-10-16T13:29:17.588Z',
   end_at: '2025-10-16T13:29:17.588Z',
-  lectures: [
-    {
-      id: 1,
-      title: 'React 기초 다지기',
-      instructor: '박개발',
-    },
-  ],
+  lectures: [],
 }

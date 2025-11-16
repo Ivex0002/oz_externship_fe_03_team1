bf60a11e8cec44ce823d00ac028da039d78780d1
yeunji0930 <yeunji0930@gmail.com>
2025-11-16 21:42:59 +0900

    fix: 빌드오류 수정

--- src/assets/dummyData/dummyStudyGroup.ts
+++ src/assets/dummyData/dummyStudyGroup.ts
@@ -2,10 +2,19 @@ import type { StudyGroupForm } from '../../types/StudyGroupTypes'
 
 export const studyGroupFormMock: StudyGroupForm = {
   name: 'React 완벽 마스터 스터디',
-  description: '함께 공부하며 성장하는 스터디입니다.',
-  startDate: '2025-10-25',
-  endDate: '2025-12-30',
-  maxMembers: 6,
-  lectures: ['React 완벽 마스터 강의'],
-  image: null,
+  introduction: '함께 공부하며 성장하는 스터디입니다.',
+  start_at: '2025-10-25',
+  end_at: '2025-12-30',
+  max_headcount: 6,
+  lectures: [
+    {
+      uuid: '',
+      thumbnail_img_url: '',
+      title: '',
+      instructor: '',
+      url_link: '',
+    },
+  ],
+  profile_img_url: null,
+  status: 'PENDING',
 }
